03ca9b6906eafcca921c919ea285bbe5716c51c1
honorke <1746562783@qq.com>
2019-11-19 13:52:02 +0800

    数据库修改

--- bookstore_app/db/seeds.rb
+++ bookstore_app/db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# books_list = [
+#
+#   ["title":" 计算机程序设计艺术 卷1：基本算法 （英文版?第3版）(计算机大师高德纳权威著作，精装版)", "price":94.00, "author":"戴士剑","publish": "机械工业出版社", "time":"2010-10-01","path":"D:/PictureList/20698869-1_b_0.jpg","picLocation":"http://img3m9.ddimg.cn/83/25/20949869-1_b_2.jpg"]
+#
+#
+# ["title":"计算机科学概论（原书第5版）", "price":62.40, "author":"韦瑟罗尔","publish": "机械工业出版社","time":"2016-05-16","path":"D:/PictureList/23962908-1_b_2.jpg","picLocation":"http://img3m8.ddimg.cn/57/6/23962908-1_b_2.jpg" ]
+#
+# ]
+
+
+# books_list.each  do |title, price, author, publish, time, path, picLocation|
+#   Book.create( title: title, price: price, author: author, publish: publish, time: time, path: path, picLocation： picLocation)
+#
+# end
+
+# books_list.each {|title, price, author, publish, time, path, picLocation|   Book.create( title: title, price: price, author: author, publish: publish, time: time, path: path, picLocation: picLocation ) }
+
+Book.create!(title: " 计算机程序设计艺术",
+             price: 94.00,
+             author: "戴士剑",
+             publish: "机械工业出版社",
+             time: "2010-10-01",
+             path: "D:/PictureList/20698869-1_b_0.jpg",
+             picLocation: "http://img3m9.ddimg.cn/83/25/20949869-1_b_2.jpg")
+
+
+
+
